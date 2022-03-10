f4a63389f3ebc8af4e1446f8ceafb78fb996be12
saifulemon <463saiful0@gmail.com>
2022-03-11 02:42:02 +0600

    Education and Skills section added

--- js/app.js
+++ js/app.js
@@ -1,8 +1,18 @@
+// for ripples
 $(document).ready(function() {
-
+// for ripples
     $('#profile-ripple').ripples({
         resolation: 512,
         dropRadius: 20
     })
 
+
+// for progress bar
+    const bars = document.querySelectorAll('.progress-bar');
+    bars.forEach(function (bar) {
+        let percentage = bar.dataset.percent;
+        let tooltip = bar.children[0];
+        tooltip.innerText = percentage + '%';
+        bar.style.width = percentage + '%';
+    })
 });
